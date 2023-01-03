081b1e6fc912fcea8f64c03dd8fc924bfe17750f
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-01-03 18:48:50 +0200

    redirects after post

--- todoapp/lists/tests.py
+++ todoapp/lists/tests.py
@@ -8,6 +8,23 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
+    def test_can_save_POST_request(self):
+        text = 'A new list item'
+        self.client.post('/', data={'item_text': text})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, text)
+
+    def test_redirects_after_POST(self):
+        text = 'A new list item'
+        response = self.client.post('/', data={'item_text': text})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -15,10 +32,6 @@ class HomePageTest(TestCase):
     def test_homepage_return_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_post_method(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
 
 
 class ItemModelTest(TestCase):
